ae9888b311d37406e929997555cd8bf5747949c9
Taylor Fausak <taylor@fausak.me>
2015-03-27 08:34:19 -0500

    Document functions and operators

--- library/Overture.hs
+++ library/Overture.hs
@@ -1,111 +1,201 @@
 module Overture where
 
+-- * Function composition
+
 {- |
+    <https://en.wikipedia.org/wiki/Function_composition Function composition>.
+
+    Use this function to combine two other functions. The result of
+    @'compose' f g@ will be a new function that first applies @f@ and then
+    applies @g@. In other words, @'compose' f g x@ is the same as @g (f x)@.
+    For instance, the following example will first add one and then multiply
+    by two.
+
     >>> let f = compose (+ 1) (* 2)
     >>> f 3
     8
 
+    You can compose many functions together, but it quickly becomes unwieldy.
+    This example does what the previous one did and then cubes the result.
+
     >>> let g = compose (compose (+ 1) (* 2)) (^ 3)
     >>> g 3
     512
+
+    This is like the function form of the '.' operator from the "Prelude".
 -}
 compose :: (a -> b) -> (b -> c) -> (a -> c)
 compose f g = \ x -> g (f x)
 
 {- |
+    Left-associative 'compose' operator.
+
+    Use this operator to combine two other functions in a more natural way
+    than with 'compose'. The result of @f '.>' g@ will be a new function that
+    first applies @f@ and then applies @g@. Here is the same example from
+    above rewritten using this operator.
+
     >>> let f = (+ 1) .> (* 2)
     >>> f 3
     8
 
+    When reading code, it is useful to pronounce this operator as "and then".
+    So the above example could be read as: Add one, /and then/ multiply by
+    two".
+
+    When composing many functions, it's easier to use this operator than
+    'compose'. Compare this with the earlier example.
+
     >>> let g = (+ 1) .> (* 2) .> (^ 3)
     >>> g 3
     512
+
+    This is like a flipped version of '.' operator from the "Prelude".
 -}
 infixl 9 .>
 (.>) :: (a -> b) -> (b -> c) -> (a -> c)
 f .> g = compose f g
 
 {- |
+    Right-associative 'compose' operator.
+
+    Sometimes it is more convenient to combine functions in the opposite
+    direction as '.>'. The result of @g '<.' f@ will be a new function that
+    applies @g@ but first applies @f@. Here is the same example from before
+    rewritten again.
+
     >>> let f = (* 2) <. (+ 1)
     >>> f 3
     8
 
+    When reading code, it is useful to pronounce this operator as "but first".
+    So the above example could be read as: "Multiply by two, /but first/ add
+    one".
+
+    Using this operator also leads to more readable code than using 'compose'
+    with many functions.
+
     >>> let g = (^ 3) <. (* 2) <. (+ 1)
     >>> g 3
     512
+
+    This is like the '.' operator from the "Prelude".
 -}
 infixr 9 <.
 (<.) :: (b -> c) -> (a -> b) -> (a -> c)
 g <. f = compose f g
 
+-- * Function application
+
 {- |
-    >>> apply 3 (+ 1)
-    4
+    <https://en.wikipedia.org/wiki/Function_application Function application>.
+
+    This function isn't usually necessary since @'apply' x f@ is the same as
+    @f x@.
+
+    >>> apply (apply 3 (+ 1)) (* 2)
+    8
+
+    However it can come in handy when working with higher-order functions.
 
     >>> map (apply 3) [(+ 1), (* 2)]
     [4,6]
+
+    This is like the '$' operator from the "Prelude".
 -}
 apply :: a -> (a -> b) -> b
 apply x f = f x
 
 {- |
-    >>> 3 |> (+ 1)
-    4
+    Left-associative 'apply' operator.
+
+    Since this operator has such low precedence, it can be used to remove
+    parentheses in complicated expressions.
+
+    >>> 3 |> (+ 1) |> (* 2)
+    8
+
+    When reading code, it is useful to pronounce this operator as "pipe into".
+    So the above example can be read as: "Three /piped into/ plus one,
+    /piped into/ times two".
+
+    It can also be used with higher-order functions, although 'apply' might be
+    clearer.
 
     >>> map (3 |>) [(+ 1), (* 2)]
     [4,6]
+
+    This is like a flipped version of the '$' operator from the "Prelude".
 -}
 infixl 0 |>
 (|>) :: a -> (a -> b) -> b
 x |> f = apply x f
 
 {- |
-    >>> (+ 1) <| 3
-    4
+    Right-associative 'apply' operator.
+
+    Like '|>', this operator also has low precedence. Use it to remove
+    parentheses.
+
+    >>> (* 2) <| (+ 1) <| 3
+    8
+
+    When reading code, it is useful to pronounce this operator as "pipe from".
+    So the above example can be read as: "Times two /piped from/ plus one,
+    /piped from/ 3".
+
+    With higher-order functions, it can be a convenient alternative to
+    @flip 'apply'@.
 
     >>> map (<| 3) [(+ 1), (* 2)]
     [4,6]
+
+    This is like the '$' operator from the "Prelude".
 -}
 infixr 0 <|
 (<|) :: (a -> b) -> a -> b
 f <| x = apply x f
 
-{- |
-    >>> apply' 3 (+ 1)
-    4
+-- ** Strict function application
 
-    >>> map (apply' 3) [(+ 1), (* 2)]
-    [4,6]
+{- |
+    Strict function application.
+
+    This is the strict version of 'apply'. It evaluates its argument with
+    'seq' before applying it to the given function. In other words,
+    @'apply'' x f@ is the same as @x \`seq\` f x@.
 
     >>> apply' undefined (const 0)
     *** Exception: Prelude.undefined
+
+    This is like the '$!' operator from the "Prelude".
 -}
 apply' :: a -> (a -> b) -> b
-apply' x f = x `seq` f x
+apply' x f = x `seq` apply x f
 
 {- |
-    >>> 3 !> (+ 1)
-    4
+    Left-associative 'apply'' operator.
 
-    >>> map (3 !>) [(+ 1), (* 2)]
-    [4,6]
+    This is the strict version of the '|>' operator.
 
     >>> undefined !> const 0
     *** Exception: Prelude.undefined
+
+    This is like a flipped version of the '$!' operator from the "Prelude".
 -}
 infixl 0 !>
 (!>) :: a -> (a -> b) -> b
 x !> f = apply' x f
 
 {- |
-    >>> (+ 1) <! 3
-    4
+    Right-associative 'apply'' operator.
 
-    >>> map (<! 3) [(+ 1), (* 2)]
-    [4,6]
+    This is the strict version of the '<!' operator.
 
     >>> const 0 <! undefined
     *** Exception: Prelude.undefined
+
+    This is like the '$!' operator from the "Prelude".
 -}
 infixr 0 <!
 (<!) :: (a -> b) -> a -> b
