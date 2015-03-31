dc3f7e4843d9ed0a39a8c9a6ad2b2499118bc683
Taylor Fausak <taylor@fausak.me>
2015-03-31 14:32:41 -0500

    Don't list source files explicitly

--- test-suite/DocTest.hs
+++ test-suite/DocTest.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
+import System.FilePath.Glob (glob)
 import Test.DocTest (doctest)
 
 main :: IO ()
-main = doctest ["library/Overture.hs"]
+main = glob "library/**/*.hs" >>= doctest
