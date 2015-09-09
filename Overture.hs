f414bf2588b3c7c79d4c731621e9f61fdd6bb469
Taylor Fausak <taylor@fausak.me>
2015-09-09 17:53:46 -0500

    Add categories

--- Overture.hs
+++ Overture.hs
@@ -1,1 +1,28 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Overture where
+
+-- * Functions
+
+flip :: (a -> b -> c) -> (b -> a -> c)
+flip f = \ x y -> f y x
+
+-- * Classes
+
+-- ** Categories
+
+class Category g where
+    identity :: g a a
+
+    compose :: g a b -> g b c -> g a c
+
+instance Category (->) where
+    identity x = x
+
+    compose f g = \ x -> g (f x)
+
+(.>) :: (Category g) => g a b -> g b c -> g a c
+(.>) = compose
+
+(<.) :: (Category g) => g b c -> g a b -> g a c
+(<.) = flip (.>)
